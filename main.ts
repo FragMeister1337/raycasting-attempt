333332153c0d9f94089ce363315b9c030b1b25e1
FragMeister1337 <80698393+FragMeister1337@users.noreply.github.com>
2021-05-18 11:03:28 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,60 +1,32 @@
 namespace SpriteKind {
     export const cam = SpriteKind.create()
+    export const frame = SpriteKind.create()
 }
-controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
-    mySprite.x = 0
-})
-function doSomething (num: number, color: number) {
+function doSomething (num: number) {
+    dist = 0
     for (let index = 0; index <= 9; index++) {
-        picture.fillRect(num * 10, index + 0 * 10, 10, 120, color)
+        picture.fillRect(num * 10, dist, 10, 120, 2)
     }
 }
-function calculate (deg: number) {
-	
-}
-let mySprite: Sprite = null
+let dist = 0
 let picture: Image = null
 picture = image.create(160, 120)
-let mySprite2 = sprites.create(img`
-    3 
-    `, SpriteKind.cam)
-mySprite2.setPosition(25, 26)
-let parameter = 0
-let spagheetti = 0
-tiles.setTilemap(tilemap`level2`)
-mySprite = sprites.create(picture, SpriteKind.Player)
+let mySprite = sprites.create(picture, SpriteKind.frame)
 game.onUpdateInterval(500, function () {
-    let column = 0
-    calculate(0)
-    doSomething(0, column)
-    calculate(1)
-    doSomething(1, column)
-    calculate(2)
-    doSomething(2, column)
-    calculate(3)
-    doSomething(3, column)
-    calculate(4)
-    doSomething(4, column)
-    calculate(5)
-    doSomething(5, column)
-    calculate(6)
-    doSomething(6, column)
-    calculate(7)
-    doSomething(7, column)
-    calculate(8)
-    doSomething(8, column)
-    calculate(9)
-    doSomething(9, column)
-    calculate(10)
-    doSomething(10, column)
-    calculate(11)
-    doSomething(11, column)
-    calculate(12)
-    doSomething(12, column)
-    calculate(13)
-    doSomething(13, column)
-    calculate(14)
-    doSomething(14, column)
-    calculate(15)
-    doSomething(15, column)
+    doSomething(0)
+    doSomething(1)
+    doSomething(2)
+    doSomething(3)
+    doSomething(4)
+    doSomething(5)
+    doSomething(6)
+    doSomething(7)
+    doSomething(8)
+    doSomething(9)
+    doSomething(10)
+    doSomething(11)
+    doSomething(12)
+    doSomething(13)
+    doSomething(14)
+    doSomething(15)
 })
