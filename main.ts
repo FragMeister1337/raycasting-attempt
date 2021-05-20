afb18032600c519d4355ce67d61c22ae4b3e882b
FragMeister1337 <80698393+FragMeister1337@users.noreply.github.com>
2021-05-20 11:08:32 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,26 @@ namespace SpriteKind {
     export const cam = SpriteKind.create()
     export const frame = SpriteKind.create()
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    myDart.throwDart()
+})
 function doSomething (num: number) {
-    dist = 0
-    for (let index = 0; index <= 9; index++) {
-        picture.fillRect(num * 10, dist, 10, 120, 2)
-    }
+	
 }
-let dist = 0
-let picture: Image = null
-picture = image.create(160, 120)
+let myDart: Dart = null
+let picture = image.create(160, 120)
 let mySprite = sprites.create(picture, SpriteKind.frame)
+let mySprite2 = sprites.create(img`
+    1 
+    `, SpriteKind.Player)
+tiles.setTilemap(tilemap`level13`)
+controller.moveSprite(mySprite2, 100, 100)
+mySprite.setPosition(80, 60)
+scene.cameraFollowSprite(mySprite2)
+myDart = darts.create(img`
+    1 
+    `, SpriteKind.Player)
+myDart.angle = 45
 game.onUpdateInterval(500, function () {
     doSomething(0)
     doSomething(1)
