ea1f88eee11521d0d699e4eb21aa99b6d32fdc30
FragMeister1337 <80698393+FragMeister1337@users.noreply.github.com>
2021-05-21 11:06:28 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,26 +2,52 @@ namespace SpriteKind {
     export const cam = SpriteKind.create()
     export const frame = SpriteKind.create()
 }
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    beamspeed += 5
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    myDart.throwDart()
+    eyebeam = sprites.createProjectileFromSprite(img`
+        1 
+        `, mySprite2, 0, beamspeed)
+    eyebeam2 = sprites.createProjectileFromSprite(img`
+        1 
+        `, mySprite2, beamspeed, 0)
+    eyebeam3 = sprites.createProjectileFromSprite(img`
+        1 
+        `, mySprite2, Math.sqrt(beamspeed * beamspeed) / 2, Math.sqrt(beamspeed * beamspeed) / 2)
+    eyebeam4 = sprites.createProjectileFromSprite(img`
+        1 
+        `, mySprite2, Math.sqrt(beamspeed * beamspeed) / 2 - 22.5 / 90 * beamspeed, Math.sqrt(beamspeed * beamspeed) / 2 + 22.5 / 90 * beamspeed)
+    eyebeam5 = sprites.createProjectileFromSprite(img`
+        1 
+        `, mySprite2, Math.sqrt(beamspeed * beamspeed) / 2 + 22.5 / 90 * beamspeed, Math.sqrt(beamspeed * beamspeed) / 2 - 22.5 / 90 * beamspeed)
 })
 function doSomething (num: number) {
 	
 }
-let myDart: Dart = null
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    beamspeed += -5
+})
+let eyebeam5: Sprite = null
+let eyebeam4: Sprite = null
+let eyebeam3: Sprite = null
+let eyebeam2: Sprite = null
+let eyebeam: Sprite = null
+let beamspeed = 0
+let mySprite2: Sprite = null
 let picture = image.create(160, 120)
 let mySprite = sprites.create(picture, SpriteKind.frame)
-let mySprite2 = sprites.create(img`
+mySprite2 = sprites.create(img`
     1 
     `, SpriteKind.Player)
 tiles.setTilemap(tilemap`level13`)
-controller.moveSprite(mySprite2, 100, 100)
 mySprite.setPosition(80, 60)
+mySprite2.setPosition(17, 17)
 scene.cameraFollowSprite(mySprite2)
-myDart = darts.create(img`
-    1 
-    `, SpriteKind.Player)
-myDart.angle = 45
+beamspeed = 50
+forever(function () {
+    info.setScore(beamspeed)
+})
 game.onUpdateInterval(500, function () {
     doSomething(0)
     doSomething(1)
